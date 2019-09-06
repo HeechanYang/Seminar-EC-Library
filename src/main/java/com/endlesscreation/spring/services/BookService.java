6d4743551a700cfe0667977475cfaf2cfcbfb4b0
HeechanYang <yhc944@gmail.com>
2019-09-07 01:44:18 +0900

    [ADD] In-Memory Services

--- src/main/java/com/endlesscreation/spring/services/BookService.java
+++ src/main/java/com/endlesscreation/spring/services/BookService.java
@@ -1,41 +1,113 @@
 package com.endlesscreation.spring.services;
 
-import com.endlesscreation.spring.mappers.BookMapper;
 import com.endlesscreation.spring.models.Book;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BookService {
 
-    private final BookMapper bookMapper;
+    private List<Book> books;
 
-    public BookService(BookMapper bookMapper) {
-        this.bookMapper = bookMapper;
+    public BookService() {
+        books = new ArrayList<>();
     }
 
-    public List<Book> getAllBooks(){
-        return bookMapper.getAllBooks();
+    public List<Book> getAllBooks() {
+        return this.books;
     }
 
-    public Book getBookById(int bookId){
-        return bookMapper.getBookById(bookId);
+    public Book getBookById(int id) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getId() == id) {
+                return books.get(i);
+            }
+        }
+
+        return null;
     }
 
-    public List<Book> getBooksByName(String name){
-        return bookMapper.getBooksByName(name);
+    public List<Book> getBooksByName(String name) {
+        List<Book> result = new ArrayList<>();
+
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getName().contains(name)) {
+                result.add(books.get(i));
+            }
+        }
+
+        return result;
     }
 
-    public int insertBook(Book book){
-        return bookMapper.insertBook(book);
+    public int insertBook(Book book) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getId() == book.getId()) {
+                // 이미 존재하는 id면 -1 반환
+                return -1;
+            }
+        }
+
+        books.add(book);
+
+        return books.size();
     }
 
-    public int updateBook(Book book){
-        return bookMapper.updateBook(book);
+    public int updateBook(Book book) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getId() == book.getId()) {
+                books.get(i).setName(book.getName());
+                books.get(i).setAuthor(book.getAuthor());
+                // 업데이트 되었으면 index 반환
+                return i;
+            }
+        }
+
+        // 업데이트된 것이 없으면 0 반환
+        return 0;
     }
 
-    public int deleteBook(int bookId){
-        return bookMapper.deleteBook(bookId);
+    public int deleteBook(int id) {
+        for (int i = 0; i < books.size(); i++) {
+            if (books.get(i).getId() == id) {
+                books.remove(i);
+                // 삭제 되었으면 index 반환
+                return i;
+            }
+        }
+
+        // 삭제된 것이 없으면 0 반환
+        return 0;
     }
+
+//    private final BookMapper bookMapper;
+//
+//    public BookService(BookMapper bookMapper) {
+//        this.bookMapper = bookMapper;
+//    }
+//
+//    public List<Book> getAllBooks(){
+//        return bookMapper.getAllBooks();
+//    }
+//
+//    public Book getBookById(int bookId){
+//        return bookMapper.getBookById(bookId);
+//    }
+//
+//    public List<Book> getBooksByName(String name){
+//        return bookMapper.getBooksByName(name);
+//    }
+//
+//    public int insertBook(Book book){
+//        return bookMapper.insertBook(book);
+//    }
+//
+//    public int updateBook(Book book){
+//        return bookMapper.updateBook(book);
+//    }
+//
+//    public int deleteBook(int bookId){
+//        return bookMapper.deleteBook(bookId);
+//    }
 }
--- src/main/java/com/endlesscreation/spring/services/MemberService.java
+++ src/main/java/com/endlesscreation/spring/services/MemberService.java
@@ -1,41 +1,113 @@
 package com.endlesscreation.spring.services;
 
-import com.endlesscreation.spring.mappers.MemberMapper;
 import com.endlesscreation.spring.models.Member;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class MemberService {
 
-    private final MemberMapper memberMapper;
+    private List<Member> members;
 
-    public MemberService(MemberMapper memberMapper) {
-        this.memberMapper = memberMapper;
+    public MemberService() {
+        members = new ArrayList<>();
     }
 
-    public List<Member> getAllMembers(){
-        return memberMapper.getAllMembers();
+    public List<Member> getAllMembers() {
+        return this.members;
     }
 
-    public Member getMemberById(String id){
-        return memberMapper.getMemberById(id);
+    public Member getMemberById(String id) {
+        for (int i = 0; i < members.size(); i++) {
+            if (members.get(i).getId().equals(id)) {
+                return members.get(i);
+            }
+        }
+
+        return null;
     }
 
-    public List<Member> getMembersByName(String name){
-        return memberMapper.getMembersByName(name);
+    public List<Member> getMembersByName(String name) {
+        List<Member> result = new ArrayList<>();
+
+        for (int i = 0; i < members.size(); i++) {
+            if (members.get(i).getName().contains(name)) {
+                result.add(members.get(i));
+            }
+        }
+
+        return result;
     }
 
-    public int insertMember(Member member){
-        return memberMapper.insertMember(member);
+    public int insertMember(Member member) {
+        for (int i = 0; i < members.size(); i++) {
+            if (members.get(i).getId().equals(member.getId())) {
+                // 이미 존재하는 id면 -1 반환
+                return -1;
+            }
+        }
+
+        members.add(member);
+
+        return members.size();
     }
 
-    public int updateMember(Member member){
-        return memberMapper.updateMember(member);
+    public int updateMember(Member member) {
+        for (int i = 0; i < members.size(); i++) {
+            if (members.get(i).getId().equals(member.getId())) {
+                members.get(i).setName(member.getName());
+                members.get(i).setContacts(member.getContacts());
+                // 업데이트 되었으면 index 반환
+                return i;
+            }
+        }
+
+        // 업데이트된 것이 없으면 0 반환
+        return 0;
     }
 
-    public int deleteMember(String id){
-        return memberMapper.deleteMember(id);
+    public int deleteMember(String id) {
+        for (int i = 0; i < members.size(); i++) {
+            if (members.get(i).getId().equals(id)) {
+                members.remove(i);
+                // 삭제 되었으면 index 반환
+                return i;
+            }
+        }
+
+        // 삭제된 것이 없으면 0 반환
+        return 0;
     }
+
+//    private final MemberMapper memberMapper;
+//
+//    public MemberService(MemberMapper memberMapper) {
+//        this.memberMapper = memberMapper;
+//    }
+//
+//    public List<Member> getAllMembers(){
+//        return memberMapper.getAllMembers();
+//    }
+//
+//    public Member getMemberById(String id){
+//        return memberMapper.getMemberById(id);
+//    }
+//
+//    public List<Member> getMembersByName(String name){
+//        return memberMapper.getMembersByName(name);
+//    }
+//
+//    public int insertMember(Member member){
+//        return memberMapper.insertMember(member);
+//    }
+//
+//    public int updateMember(Member member){
+//        return memberMapper.updateMember(member);
+//    }
+//
+//    public int deleteMember(String id){
+//        return memberMapper.deleteMember(id);
+//    }
 }
